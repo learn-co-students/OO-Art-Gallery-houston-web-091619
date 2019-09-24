dfba30939bc2be21b7e3d26d520a8bd7fcc962fe
alexiscash <alexiscash97@rocketmail.com>
2019-09-24 17:08:48 -0500

    done

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,42 @@
 class Gallery
 
+  @@all = []
+
   attr_reader :name, :city
 
   def initialize(name, city)
     @name = name
     @city = city
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |i|
+      i.gallery == self
+    end
+  end
+
+  def artists
+    self.paintings.map do |i|
+      i.artist
+    end.uniq
+  end
+
+  def artist_names
+    self.artists.map do |i|
+      i.name
+    end
+  end
+
+  def most_expensive_painting
+    self.paintings.max_by do |i|
+      i.price
+    end
   end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,26 @@
 class Painting
 
-  attr_reader :title, :price
-
   @@all = []
 
-  def initialize(title, price)
+  attr_reader :title, :price, :artist, :gallery
+
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    self.all.map(&:price).reduce(&:+)
+  end
+  
 end
+
+
