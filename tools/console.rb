d6e2915916cd669afbc6be13589eef433f81e680
Damini Varu <damvaru4@gmail.com>
2019-09-25 02:04:18 +0000

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,19 @@
-require_relative '../config/environment.rb'
+require_relative '/Users/daminivaru/OO-Art-Gallery/config/environment.rb'
+require 'pry'
 
+a1 = Artist.new("Monet", 10.0)
+a2 = Artist.new("Dali", 50.0)
+a3 = Artist.new("Patrick", 60.0)
+
+
+g1 = Gallery.new("tate", "london")
+g2 = Gallery.new("Louve", "Paris")
+g3 = Gallery.new("Angel", "Houston")
+
+
+p1 = Painting.new("lillies", 1000, a1, g1)
+p2 = Painting.new("Clocks", 2000, a2, g2)
+p3 = Painting.new("Panini", 3000, a3, g3)
 
 
 
